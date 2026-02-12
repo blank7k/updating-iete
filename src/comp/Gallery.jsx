338889a662af77e8d22bc535678c09edb72be891
Rohit prasad <72071869+blank7k@users.noreply.github.com>
2026-02-13 03:50:31 +0530

    Update Gallery images and fix slider styling

--- src/comp/Gallery.jsx
+++ src/comp/Gallery.jsx
@@ -22,37 +22,75 @@ const Gallery = () => {
     {
       title: "Research Paper Writing Seminar",
       date: "2025-26",
-      images: manualImages,
+      images: [
+        "https://i.postimg.cc/MzMWhdDG/IMG-6797.avif",
+        "https://i.postimg.cc/CMTPxLTg/IMG-6795.avif"
+      ],
       redirectUrl: ""
     },
     {
       title: "Mosaic 2k25",
       date: "2025-26",
-      images: manualImages,
+      images: [
+        "https://i.postimg.cc/HdmvGx9k/PXL-20251017-045139692-MP-jpg.jpg",
+        "https://i.postimg.cc/0xDXHmDR/IMG-20251017-122836-jpg.jpg",
+        "https://i.postimg.cc/37mfbppC/IMG-20251017-122750-jpg.jpg",
+        "https://i.postimg.cc/NtWP4by1/IMG-20251017-122237-jpg.jpg",
+        "https://i.postimg.cc/xQWFRgkm/IMG-7103.avif",
+        "https://i.postimg.cc/59S39BSx/IMG_7183.avif"
+      ],
       redirectUrl: ""
     },
     {
       title: "MimoVerse",
       date: "2025-26",
-      images: manualImages,
+      images: [
+        "https://i.postimg.cc/mTfm3rts/Screenshot-(133).png",
+        "https://i.postimg.cc/6BJMVQyt/Screenshot-(134).png",
+        "https://i.postimg.cc/ZJtwPqCR/Screenshot-(136).png",
+        "https://i.postimg.cc/x9D6Gdc0/Screenshot-(143).png",
+        "https://i.postimg.cc/5fBnsMC6/Screenshot-(147).png",
+        "https://i.postimg.cc/zqC0x1gS/Screenshot-(152).png"
+      ],
       redirectUrl: ""
     },
     {
       title: "IOT/IIOT",
       date: "2025-26",
-      images: manualImages,
+      images: [
+        "https://i.postimg.cc/442rNR5z/IMG_20251001_WA0078_jpg.jpg",
+        "https://i.postimg.cc/2ytJHn68/IMG_6171_(1).avif",
+        "https://i.postimg.cc/hvv6b2SK/IMG_6219.avif",
+        "https://i.postimg.cc/PrL0CV3L/IMG_20251001_WA0119_jpg.jpg"
+      ],
       redirectUrl: ""
     },
     {
       title: "IETE Farewell 25-26",
       date: "2025-26",
-      images: manualImages,
+      images: [
+        "https://i.postimg.cc/gctfkbDc/IMG_20250806_WA0094_jpg.jpg",
+        "https://i.postimg.cc/mDBtDHTj/IMG_6081_JPG.jpg",
+        "https://i.postimg.cc/VktyVZXL/IMG_6082_JPG.jpg",
+        "https://i.postimg.cc/t460MvFb/IMG_6085_JPG.jpg",
+        "https://i.postimg.cc/fRd4rqYw/IMG_6086_JPG.jpg",
+        "https://i.postimg.cc/Cx8yQrbM/IMG_6088_JPG.jpg",
+        "https://i.postimg.cc/yNhMVJQx/IMG_6089_JPG.jpg",
+        "https://i.postimg.cc/DZ2tm2Hm/IMG_6205_JPG.jpg"
+      ],
       redirectUrl: ""
     },
     {
       title: "IETE Freshers 25-26",
       date: "2025-26",
-      images: manualImages,
+      images: [
+        "https://i.postimg.cc/zvPB02Kb/IMG_6219_JPG.jpg",
+        "https://i.postimg.cc/fygLBqmX/IMG_6223_JPG.jpg",
+        "https://i.postimg.cc/MHrTd3V1/IMG_6241_JPG.jpg",
+        "https://i.postimg.cc/T1HwCNgb/IMG_6245_JPG.jpg",
+        "https://i.postimg.cc/pVcgXMZ5/IMG_20250806_WA0342_jpg.jpg",
+        "https://i.postimg.cc/xjscf8hT/IMG_6309_JPG.jpg"
+      ],
       redirectUrl: ""
     }
   ];
